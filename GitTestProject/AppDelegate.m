64e18b7a75ec58bf2de7810014eab49a2a68bb09
lisa liu <emily820522@163.com>
2016-02-18 16:47:39 +0800

    initial git modify test

--- GitTestProject/AppDelegate.m
+++ GitTestProject/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"git project test here");
     return YES;
 }
 
