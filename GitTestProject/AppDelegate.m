46a3a598a8900702e8774b290c81bc5cbd4faeae
lisa liu <emily820522@163.com>
2016-02-19 14:01:33 +0800

    test git commit without add operation

--- GitTestProject/AppDelegate.m
+++ GitTestProject/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"git status diff testing");
+    NSLog(@"git status diff testing!");
     return YES;
 }
 
