bff42389d7f72e96abb382afedf65a27fc8ba460
lisa liu <emily820522@163.com>
2016-02-19 15:27:35 +0800

    Test git mv command

---
+++ GitTestProject/GitTest2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  GitTestViewController.h
+//  GitTestProject
+//
+//  Created by lisa liu on 2/19/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitTestViewController : UIViewController
+
+@end
---
+++ GitTestProject/GitTest2ViewController.m
@@ -0,0 +1,13 @@
+//
+//  GitTestViewController.m
+//  GitTestProject
+//
+//  Created by lisa liu on 2/19/16.
+//
+//
+
+#import "GitTestViewController.h"
+
+@implementation GitTestViewController
+
+@end
