f4dc4be6d6ce691f8c79e07cdc00b6683e5959ce
lisa liu <emily820522@163.com>
2016-02-19 14:27:08 +0800

    Test git rm command

---
+++ GitTestProject/GitTestFile.h
@@ -0,0 +1,13 @@
+//
+//  GitTestFile.h
+//  GitTestProject
+//
+//  Created by lisa liu on 2/19/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitTestFile : UIViewController
+
+@end
---
+++ GitTestProject/GitTestFile.m
@@ -0,0 +1,13 @@
+//
+//  GitTestFile.m
+//  GitTestProject
+//
+//  Created by lisa liu on 2/19/16.
+//
+//
+
+#import "GitTestFile.h"
+
+@implementation GitTestFile
+
+@end
