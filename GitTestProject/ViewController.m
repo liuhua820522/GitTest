af4fb5e9239a097829703ca6de764e7cfe307b5c
lisa liu <emily820522@163.com>
2016-02-23 17:08:04 +0800

    testing on master

--- GitTestProject/ViewController.m
+++ GitTestProject/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"here is a testing");
+    NSLog(@"here is a testing master");
 }
 
 - (void)didReceiveMemoryWarning {
